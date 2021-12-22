6498511f06063e95fd22f208b8d019256e6251ec
Kranthi <kranthi@goodyear.com>
2021-12-22 16:06:19 -0500

    Added code for DNA app - which takes in dna sequence input.

--- 1_simple_stock_price/main_stock_price.py
+++ 1_simple_stock_price/main_stock_price.py
@@ -1,12 +1,14 @@
 import yfinance as yf
 import streamlit as st
 
-st.write("""
+st.write(
+    """
 # Simple Stock Price Application
 
 Shown are the stock ***closing price*** and ***volume*** of Goodyear!
 
-""")
+"""
+)
 
 ticker_symbol = "GOOGL"
 ticker_data = yf.Ticker(ticker_symbol)
@@ -15,13 +17,16 @@ ticker_data = yf.Ticker(ticker_symbol)
 ticker_df = ticker_data.history(period="1d", start="2010-5-31", end="2021-12-31")
 
 # Plot the values
-st.write("""
+st.write(
+    """
 ## Closing Price
-""")
+"""
+)
 st.line_chart(ticker_df.Close)
 
-st.write("""
+st.write(
+    """
 ## Volume
-""")
+"""
+)
 st.line_chart(ticker_df.Volume)
-
