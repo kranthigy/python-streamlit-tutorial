9a5c7faf17f5693d32f34274bad9af17bad9b399
Kranthi <kranthi@goodyear.com>
2021-12-22 12:26:47 -0500

    Add labels for the plots.

--- 1_simple_stock_price/main_stock_price.py
+++ 1_simple_stock_price/main_stock_price.py
@@ -4,15 +4,24 @@ import streamlit as st
 st.write("""
 # Simple Stock Price Application
 
-Shown are the stock closing price and volume of Goodyear!
+Shown are the stock ***closing price*** and ***volume*** of Goodyear!
 
 """)
 
-ticker_symbol = "GT"
+ticker_symbol = "GOOGL"
 ticker_data = yf.Ticker(ticker_symbol)
+
 
 ticker_df = ticker_data.history(period="1d", start="2010-5-31", end="2021-12-31")
 
+# Plot the values
+st.write("""
+## Closing Price
+""")
 st.line_chart(ticker_df.Close)
+
+st.write("""
+## Volume
+""")
 st.line_chart(ticker_df.Volume)
 
